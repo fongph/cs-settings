07af46724b5739de986139bf9cdef29e9a75b1f8
Orest B <b.orest@dizboard.com>
2015-04-01 07:43:25 +0000

    update ios app version to 4

--- src/GlobalSettings.php
+++ src/GlobalSettings.php
@@ -137,7 +137,7 @@ class GlobalSettings
     protected static $removeApp = false; //if deleteApp = true then android and ios replace self empty app
     
     protected static $versionsApps = array(
-        'ios' => 3,
+        'ios' => 4,
         'android' => 6,
         'blackberry' => 1
     );
