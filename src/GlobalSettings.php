7019035b8ccf49a04f6529ae772a250992e281ea
Orest B <b.orest@dizboard.com>
2015-05-26 08:35:50 +0000

    update android version to 9

--- src/GlobalSettings.php
+++ src/GlobalSettings.php
@@ -138,7 +138,7 @@ class GlobalSettings
     
     protected static $versionsApps = array(
         'ios' => 6,
-        'android' => 8,
+        'android' => 9,
         'blackberry' => 1
     );
     protected static $apiJson = array(
