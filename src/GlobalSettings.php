beb110028612859dd54a54372f845198860b4fb7
NSergey <n.sergey@dizboard.com>
2015-08-13 11:04:29 +0300

    update version android 14 => 15

--- src/GlobalSettings.php
+++ src/GlobalSettings.php
@@ -141,7 +141,7 @@ class GlobalSettings
     
     protected static $versionsApps = array(
         'ios' => 9,
-        'android' => 14,
+        'android' => 15,
         'blackberry' => 1
     );
     protected static $apiJson = array(
