848589042eef7f820244af7320271e74b18bb8fc
Orest B <b.orest@dizboard.com>
2016-03-18 14:44:02 +0000

    update android version to 23

--- src/GlobalSettings.php
+++ src/GlobalSettings.php
@@ -141,7 +141,7 @@ class GlobalSettings
     
     protected static $versionsApps = array(
         'ios' => 19,
-        'android' => 19,
+        'android' => 23,
         'blackberry' => 1
     );
     protected static $apiJson = array(
