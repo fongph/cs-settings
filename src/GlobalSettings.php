6263af370df96d4bdb732f788d543e06a339cd8d
b.orest <b.orest@dizboard.com>
2017-02-22 13:36:15 +0100

    Update GlobalSettings.php

--- src/GlobalSettings.php
+++ src/GlobalSettings.php
@@ -125,7 +125,7 @@ class GlobalSettings
         'PresetId' => '1351620000001-000020'
     );
     protected static $cloudFront = array(
-        'domain' => 'https://media.pumpic.com/',
+        'domain' => 'http://media.pumpic.com/',
         'keyPairId' => 'APKAJGHJLY2FNTE5A62Q'
     );
     protected static $icloud = array(
